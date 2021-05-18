547694fcfda02e39bf47d515a3dafbebb1d1aea7
Yucheng Chuang <ducksteven@gmail.com>
2021-05-18 21:11:36 +0800

    Change scraper so that it produces JSON instead of uploading to Redis

--- backend/hospital_types.py
+++ backend/hospital_types.py
@@ -8,7 +8,7 @@ HospitalID = int
 class AppointmentAvailability(str, Enum):
     AVAILABLE: str = "Available"
     UNAVAILABLE: str = "Unavailable"
-    NO_DATA: str = "No data"
+    NO_DATA: str = "No Data"
 
 
 class Hospital(TypedDict):
--- backend/local_scraper.py
+++ backend/local_scraper.py
@@ -1,4 +1,9 @@
-import redis, os
+# Usage:
+# local_scraper.py > output.json
+
+import argparse
+import os
+import json
 from typing import TypedDict, Tuple, Dict, Callable, List, Any, Optional, NewType
 from enum import Enum
 from hospital_types import (
@@ -29,14 +34,6 @@ from Parsers.ncku_tainan import *
 from Parsers.kmuh_kaohsiung import *
 from Parsers.sanjunzong_penghu import *
 
-load_dotenv()
-
-redis_host: Optional[str] = os.environ.get("REDIS_HOST")
-redis_port: Optional[str] = os.environ.get("REDIS_PORT")
-redis_username: Optional[str] = os.environ.get("REDIS_USERNAME")
-redis_password: Optional[str] = os.environ.get("REDIS_PASSWORD")
-
-
 def error_boundary(
     f: Callable[[], Coroutine[Any, Any, ScrapedData]]
 ) -> Callable[[], Coroutine[Any, Any, Optional[ScrapedData]]]:
@@ -44,10 +41,10 @@ def error_boundary(
         try:
             f_start: float = time.time()
             value = await f()
-            print("----%s: %s-----" % (f.__name__, str(time.time() - f_start)))
+            print("----%s: %s-----" % (f.__name__, str(time.time() - f_start)), file=sys.stderr)
             return value
         except:
-            print("----%s: Unexpected error:" % f.__name__, sys.exc_info()[0])
+            print("----%s: Unexpected error:" % f.__name__, sys.exc_info()[0], file=sys.stderr)
             return None
 
     return boundaried_function
@@ -76,11 +73,14 @@ PARSERS: List[Callable[[], Coroutine[Any, Any, Optional[ScrapedData]]]] = [
 ]
 
 
-async def get_hospital_availability() -> List[ScrapedData]:
+# TODO: make it accepts an id of which site to scrape
+async def scrape() -> List[ScrapedData]:
     availability: List[ScrapedData] = list(
         filter(None, list(await asyncio.gather(*[f() for f in PARSERS])))
     )
     as_dict: Dict[int, HospitalAvailabilitySchema] = dict(availability)
+
+    # FIXME: use opaque ID so that we don't limit scrapers up to 32 items.
     for i in range(1, 32):
         if i in as_dict:
             continue
@@ -89,54 +89,11 @@ async def get_hospital_availability() -> List[ScrapedData]:
                 "self_paid": AppointmentAvailability.NO_DATA,
                 "government_paid": AppointmentAvailability.NO_DATA,
             }
-    print(list(as_dict.items()))
+
     return list(as_dict.items())
 
-
-async def scrape() -> None:
-    """Example Hello Redis Program"""
-
-    # step 3: create the Redis Connection object
-    try:
-
-        # The decode_repsonses flag here directs the client to convert the responses from Redis into Python strings
-        # using the default encoding utf-8.  This is client specific.
-        r: redis.StrictRedis = redis.StrictRedis(
-            host=redis_host,
-            port=redis_port,
-            password=redis_password,
-            decode_responses=True,
-            username=redis_username,
-            socket_timeout=10,
-            ssl=True,
-        )
-
-        def set_availability(
-            hospital_id: int,
-            availability: HospitalAvailabilitySchema,
-        ) -> None:
-            f: Callable[[AppointmentAvailability], str] = lambda x: x.__str__()
-            # pyre-fixme[6]: Pyre cannot detect that the objects here are AppointmentAvailability
-            primitive_availability = {k: f(v) for k, v in availability.items()}
-            r.hset(
-                "hospital_schema_2:" + str(hospital_id),
-                key=None,
-                value=None,
-                # pyre-fixme[6]: Pyre cannot make Dict[str, str] compatible with their HSet type.
-                mapping=primitive_availability,
-            )
-
-        availability = await get_hospital_availability()
-
-        [
-            set_availability(hospital_availability[0], hospital_availability[1])
-            for hospital_availability in availability
-        ]
-
-    except Exception as e:
-        print(e)
-
-
 if __name__ == "__main__":
-    asyncio.run(scrape())
-    print("--- %s seconds ---" % (time.time() - START_TIME))
+    data = asyncio.run(scrape())
+    print(json.dumps(data)) # to stdout
+
+    print("--- %s seconds ---" % (time.time() - START_TIME), file=sys.stderr)
